fd4eb02a463d99a2f1d40f777f0f4da2821f1b6f
Paula Bittencourt <paula.bittencourt@gmail.com>
2022-12-06 12:22:56 -0300

    métodos get e post

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ import express from "express";
 import * as dotenv from "dotenv";
 import dbConnect from "./config/db.config.js";
 import userRouter from "./routes/user.routes.js";
+import cors from "cors";
 
 dotenv.config();
 
 dbConnect();
 
 const app = express();
+
+// configuração da origem da aplicação react (origens cruzadas, não são iguais)
+app.use(cors({ origin: process.env.REACT_URL }));
 app.use(express.json());
 
-// 8080/user
+// 8080/user/register
 app.use("/user", userRouter);
 
 app.listen(Number(process.env.PORT), () =>
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import UserModel from "../models/user.model.js";
+import generateToken from "../config/jwt.config.js";
 
 const router = express.Router();
 const rounds = 10; // o bcryp precisa disso para saber qual é a configuração dos saltos/voltas necessários para criar o código
@@ -31,6 +32,62 @@ router.post("/register", async (request, response) => {
   } catch (error) {
     console.log(error);
     return response.status(500).json({ msg: "algo deu errado com o cadastro" });
+  }
+});
+
+// realizar o login
+router.post("/login", async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    // encontrar o usuário
+    const user = await UserModel.findOne({ email: email });
+
+    // checar se o usuário existe
+    if (!user) {
+      return response
+        .status(400)
+        .json({ msg: "Dados de autenticação inválidos" });
+    }
+
+    // comparar senhas
+    if (await bcrypt.compare(password, user.password)) {
+      delete user._doc.password;
+      const token = generateToken(user);
+
+      return response.status(200).json({
+        user: { ...user._doc },
+        token: token,
+      });
+    } else {
+      return response
+        .status(401)
+        .json({ msg: "Dados de autenticação inválidos" });
+    }
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({ msg: "algo deu errado no login" });
+  }
+});
+
+// GET do usuário logado para recuperar no front-end
+router.get("/profile", isAuth, attachCurrentUser, async (request, response) => {
+  try {
+    const loggedUser = request.currentUser;
+
+    // checar se realmente há um usuário logado
+    if (!loggedUser) {
+      return response.status(404).json({ msg: "usuário não encontrado" });
+    }
+
+    const user = await UserModel.findById(loggedUser._id);
+
+    // retirar informações sensíveis
+    delete user._doc.password;
+
+    return response.status(200).json(user);
+  } catch (error) {
+    return response.status(500).json({ msg: "algo deu errado" });
   }
 });
 
