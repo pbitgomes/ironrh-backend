e0d524af12e0abdee57b458721d115891d68afc8
tatialveso <tatiialveso@gmail.com>
2022-12-06 12:25:25 -0300

    :sparkles: create user routes for login & profile

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bcrypt from 'bcrypt'
 import UserModel from '../models/user.model.js'
+import generateToken from '../config/jwt.config.js'
 
 const router = express.Router()
 const rounds = 10
@@ -34,8 +35,56 @@ router.post("/register", async (request, response) => {
     }
 })
 
-router.post("/login", () => {
-    
+// realizar o login
+router.post("/login", async (request, response) => {
+    try {
+        const { email, password } = request.body
+
+        // encontrar o usuário
+        const user = await UserModel.findOne({ email: email })
+
+        // checar se o usuário existe ou não
+        if(!user) {
+            return response.status(400).json({ msg: "senha e e-mail não está cadastrado"})
+        }
+
+        // comparar senhas
+        if(await bcrypt.compare(password, user.password)) {
+            delete user._doc.password
+            const token = generateToken(user)
+
+            return response.status(200).json({
+                user: { ...user._doc },
+                token: token
+            })
+        } else {
+            return response.status(401).json({ msg: 'senha e e-mail não está correta'})
+        }
+    } catch (error) {
+        console.log(error)
+        return response.status(500).json({ msg: 'algo deu errado no login' })
+    }
+})
+
+// get do usuário logado para recuperar no front-end
+router.get("/profile", isAuth, attachCurrentUser, async (request, response) => {
+    try {
+        const loggedUser = request.currentUser
+
+        // checar se realmente temos um usuário logado
+        if(!loggedUser) {
+            return response.status(404).json({msg: 'usuário não encontrado'})
+        }
+
+        const user = await UserModel.findById(loggedUser._id)
+
+        // retirar informações sensíveis
+        delete user._doc.password
+
+        return response.status(200).json(user)
+    } catch (error) {
+        return response.status(500).json({ msg: 'algo deu errado'})
+    }
 })
 
 export default router
