111d6103b2995645ef7956ab4f0c9ea12e4e50e0
tatialveso <tatiialveso@gmail.com>
2022-12-06 10:18:48 -0300

    :sparkles: create user model

---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+import { model, Schema } from "mongoose"
+
+const userSchema = new Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            match: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/gm
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        role: {
+            type: String,
+            enum: ['usuário', 'admin'],
+            default: 'usuário'
+        }
+    }
+)
+
+const UserModel = model("User", userSchema)
+
+export default UserModel
