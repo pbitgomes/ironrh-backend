0bd991e71994c896864828604ad4496ccee88850
tatialveso <tatiialveso@gmail.com>
2022-12-06 12:25:35 -0300

    :sparkles: set cors

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import dbConnect from './config/db.config.js'
 import userRouter from './routes/user.routes.js'
+import cors from 'cors'
 
 dotenv.config()
 
 dbConnect()
 
 const app = express()
+// configuração da origin da aplicação react
+app.use(cors( { origin: process.env.REACT_URL } ))
 app.use(express.json())
 
 // 8080/user/register
